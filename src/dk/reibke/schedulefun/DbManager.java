7d81908742482577436d1004ce8bcd7c78d6f029
Nikolaj Reibke <nikolajsr12@gmail.com>
2013-11-27 16:07:45 +0100

    added functionality for signup including database

---
+++ src/dk/reibke/schedulefun/DbManager.java
@@ -0,0 +1,112 @@
+package dk.reibke.schedulefun;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbManager extends SQLiteOpenHelper{
+
+	private String username;
+	private SQLiteDatabase databaseWrite;
+	private SQLiteDatabase databaseRead;
+	private Context context;
+	
+	private static DbManager instance;
+	
+	public static DbManager getInstance(){
+		
+		if(instance == null){
+			return null;
+		}
+		return instance;
+	}
+	
+	public static DbManager InitiateDatabase(Context context, String name, CursorFactory factory,
+			int version, String username){
+		if(instance == null){
+			instance = new DbManager(context, name, factory, version, username);
+		}
+		return instance;
+	}
+	
+	public static DbManager getInstance(Context context){
+		if(instance != null){
+			instance.setContext(context);
+			return instance;
+			
+		}
+		return null;
+		
+	}
+	
+
+	private void setContext(Context context) {
+		this.context = context;
+		
+	}
+
+	private DbManager(Context context, String name, CursorFactory factory,
+			int version, String username) {
+		super(context, name, factory, version);
+		this.context = context;
+		this.username = username;
+		this.databaseWrite = getWritableDatabase();
+		this.databaseRead = getReadableDatabase();
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE users (username TEXT UNIQUE, " +
+				"password TEXT NOT NULL" +
+				"UId INTEGER PRIMARY KEY)");
+		db.execSQL("CREATE TABLE schedule(name TEXT, " +
+				"SId INTEGER PRIMARY KEY, " +
+				"interval INTEGER NOT NULL, " +
+				"repeatTimes INTEGER NOT NULL)");
+		db.execSQL("CREATE TABLE specielschedule(SId Integer NOT NULL, " +
+				"time INTEGER NOT NULL)");
+		db.execSQL("CREATE TABLE ids (UId INTEGER UNIQUE, SId INTEGER UNIQUE)");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean hasUser(String username) {
+		String[] fields = {"username"};
+		Cursor result = this.databaseRead.rawQuery("SELECT ? " +
+				"FROM users" +
+				"WHERE ? = " + username + ";", fields);
+		if(result.getCount() < 1){
+			return false;
+		}
+		return true;
+	}
+
+	public void addUser(String username, String password) {
+		String sql = "INSERT INTO users VALUES(?,?)";
+		SQLiteStatement statement = this.databaseWrite.compileStatement(sql);
+		this.databaseWrite.beginTransaction();
+		try{
+			statement.clearBindings();
+			statement.bindString(1, username);
+			statement.bindString(2, password);
+			statement.execute();
+			this.databaseWrite.setTransactionSuccessful();
+		}catch (SQLException e){
+			throw new Error("Something went wrong");
+		}finally{
+			this.databaseWrite.endTransaction();
+		}
+	}
+	
+}
--- src/dk/reibke/schedulefun/SignupActivity.java
+++ src/dk/reibke/schedulefun/SignupActivity.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends Activity {
 
 	private String username;
+	private EditText usernameField;
+	private EditText passwordField1;
+	private EditText passwordField2;
+	private TextView intention;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,7 @@ public class SignupActivity extends Activity {
 		setContentView(R.layout.activity_signup);
 		Intent intent = getIntent();
 		this.username = intent.getStringExtra("username");
-		
-		
+		this.intention = (TextView) findViewById(R.id.signup_message);
 	}
 
 	@Override
@@ -24,6 +30,39 @@ public class SignupActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.signup, menu);
 		return true;
+	}
+	
+	private void registerUser(View v){
+		this.usernameField = (EditText) findViewById(R.id.signup_username_field);
+		this.passwordField1 = (EditText) findViewById(R.id.signup_password_field);
+		this.passwordField2 = (EditText) findViewById(R.id.signup_password2_field);
+		
+		this.username = this.usernameField.getText().toString();
+		String pass1 = this.passwordField1.getText().toString();
+		String pass2 = this.passwordField2.getText().toString();
+		if(pass1.equals(pass2) && DbManager.getInstance(this).hasUser(this.username)){
+			DbManager.getInstance(this).addUser(this.username, pass1);
+			userAdded();
+		}else{
+			if(!pass1.equals(pass2)){
+				noPassEqual();
+			}else{
+				UserTaken();
+			}
+		}
+	}
+
+	private void userAdded() {
+		this.intention.setText("User added");
+		
+	}
+
+	private void UserTaken() {
+		this.intention.setText("Username already exists!");
+	}
+
+	private void noPassEqual() {
+		this.intention.setText("Passwords not equal!");
 	}
 
 }
